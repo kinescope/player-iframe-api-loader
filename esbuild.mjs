510a606f25d43c5c200f65a867b28a29a0e29373
Vladimir <vzh.inbox@gmail.com>
2025-03-25 16:02:12 +0500

    fix: build

--- esbuild.mjs
+++ esbuild.mjs
@@ -4,6 +4,13 @@ import esbuildPluginTsc from 'esbuild-plugin-tsc';
 await esbuild.build({
   entryPoints: ['src/react/KinescopePlayer.tsx'],
   bundle: true,
+  platform: 'browser',
+  format: 'esm',
+  target: ['esnext'],
+  keepNames: false,
+  treeShaking: true,
+  minifySyntax: true,
+  minifyIdentifiers: true,
   external: ['react', '../loader'],
   outdir: 'dist/react',
   plugins: [
