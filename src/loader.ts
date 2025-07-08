f1e7df42f0196e9122b40bf35ac7830b3b6139e2
Vladimir <vzh.inbox@gmail.com>
2025-07-08 21:45:06 +0500

    refactor: Update loop

--- src/loader.ts
+++ src/loader.ts
@@ -5,7 +5,8 @@ import type { Kinescope } from './types';
 
 function findScript(src: string): HTMLScriptElement | undefined {
   const url = src.startsWith('//') ? window.location.protocol + src : src;
-  for (let i = 0; i < document.scripts.length; i += 1) {
+  const { length } = document.scripts;
+  for (let i = 0; i < length; i += 1) {
     if (document.scripts[i].src === url) {
       return document.scripts[i];
     }
